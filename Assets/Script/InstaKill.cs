f151baebcd7e6c4bb8fd087aaf793961c6c87122
Jordan <j.drespincho@yahoo.com>
2017-11-23 15:11:27 +0100

    Update

--- Assets/Script/InstaKill.cs
+++ Assets/Script/InstaKill.cs
@@ -5,15 +5,28 @@ using UnityEngine.Assertions;
 
 public class InstaKill : MonoBehaviour {
 
+    [Header("Requirements")]
     public Transform playerTransform;
     public HUDHandler uiHandler;
 
+    [Header("Parameters")]
+    public float acceleration = 10f;
+    public float maxSpeed = 20f;
+
+
     private float _dist;
     private int _distInInt;
+    private Rigidbody _rigidbody;
+    private float _speedToAdd;
+    private float _accelerationToAdd;
 
     public void Awake()
     {
+        _speedToAdd = 0f;
+        _accelerationToAdd = 0f;
+        _rigidbody = GetComponent<Rigidbody>();
         Assert.IsNotNull(playerTransform);
+        Assert.IsNotNull(uiHandler);
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -27,11 +40,38 @@ public class InstaKill : MonoBehaviour {
 
     public void Update()
     {
-        _dist = Vector3.Distance(playerTransform.position, transform.position);
-        if(_dist <= 100f)
+        if (playerTransform)
         {
-            uiHandler.SetTransparency(1f-(_dist/100f));
+            _dist = Vector3.Distance(playerTransform.position, transform.position);
+            if (_dist <= 100f)
+            {
+                uiHandler.SetTransparency(1f - (_dist / 100f));
+            }
+            //Debug.Log(_dist);
         }
-        //Debug.Log(_dist);
+
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 newVelocity = _rigidbody.velocity;
+        if (newVelocity.z > maxSpeed + _speedToAdd)
+        {
+            newVelocity.z = maxSpeed + _speedToAdd;
+        }
+
+        else
+        {
+            newVelocity.z += (acceleration + _accelerationToAdd) * Time.fixedDeltaTime;
+        }
+
+        _rigidbody.velocity = newVelocity;
+        //Debug.Log(_rigidbody.velocity);
+    }
+
+    public void SpeedIncrease(float addSpeed, float addAcceleration)
+    {
+        _speedToAdd += addSpeed;
+        _accelerationToAdd += addAcceleration;
     }
 }
--- Assets/Script/MaxSpeedIncrease.cs
+++ Assets/Script/MaxSpeedIncrease.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MaxSpeedIncrease : MonoBehaviour {
 
+    [Header("Player Acceleration")]
     public float addSpeed = 50.0f;
     public float addAcceleration = 10.0f;
+
+    [Header("Wave Acceleration")]
+    public float addSpeedWave = 50.0f;
+    public float addAccelerationWave = 10.0f;
 
     public void OnTriggerEnter(Collider collision)
     {
@@ -14,6 +19,12 @@ public class MaxSpeedIncrease : MonoBehaviour {
         {
             player.SpeedIncrease(addSpeed, addAcceleration);
             //Destroy(this.gameObject);
+        }
+
+        InstaKill wave = collision.gameObject.GetComponent<InstaKill>();
+        if (wave)
+        {
+            wave.SpeedIncrease(addSpeedWave, addAccelerationWave);
         }
     }
 }
